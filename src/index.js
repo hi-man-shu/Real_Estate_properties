4c031c04882c3d3730eb6ded5678be3abb449064
hi-man-shu <himanshulohar34790@gmail.com>
2021-09-29 08:50:59 +0530

    files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,243 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import building from "./images/building.png";
+import "./index.css";
+class Products extends React.Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    this.state = {};
+    this.state.filterText = "";
+    this.state.products = [
+      {
+        id: 1,
+        category: "1BHK flat",
+        price: "10000₹",
+        qty: 12,
+        name: "sitapura colony",
+      },
+      {
+        id: 2,
+        category: "2 BHK flat",
+        price: "19900₹",
+        qty: 10,
+        name: "Mansarover colony",
+      },
+      {
+        id: 3,
+        category: "banglow",
+        price: "299990₹",
+        qty: 3,
+        name: "Rose Villa",
+      },
+      {
+        id: 4,
+        category: "Resort",
+        price: "99999₹",
+        qty: 5,
+        name: "THT resort",
+      },
+      {
+        id: 5,
+        category: "Hotel",
+        price: "399999₹",
+        qty: 2,
+        name: "Hotel Rockford",
+      },
+      {
+        id: 6,
+        category: "House",
+        price: "199399₹",
+        qty: 23,
+        name: "Shanti Niwas",
+      },
+    ];
+  }
+  handleUserInput(filterText) {
+    this.setState({ filterText: filterText });
+  }
+  handleRowDel(product) {
+    var index = this.state.products.indexOf(product);
+    this.state.products.splice(index, 1);
+    this.setState(this.state.products);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleAddEvent(evt) {
+    var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+    var product = {
+      id: id,
+      name: "",
+      price: "",
+      category: "",
+      qty: 0,
+    };
+    this.state.products.push(product);
+    this.setState(this.state.products);
+  }
+
+  handleProductTable(evt) {
+    var item = {
+      id: evt.target.id,
+      name: evt.target.name,
+      value: evt.target.value,
+    };
+    var products = this.state.products.slice();
+    var newProducts = products.map(function (product) {
+      for (var key in product) {
+        if (key == item.name && product.id == item.id) {
+          product[key] = item.value;
+        }
+      }
+      return product;
+    });
+    this.setState({ products: newProducts });
+    //  console.log(this.state.products);
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onUserInput={this.handleUserInput.bind(this)}
+        />
+        <ProductTable
+          onProductTableUpdate={this.handleProductTable.bind(this)}
+          onRowAdd={this.handleAddEvent.bind(this)}
+          onRowDel={this.handleRowDel.bind(this)}
+          products={this.state.products}
+          filterText={this.state.filterText}
+        />
+      </div>
+    );
+  }
+}
+class SearchBar extends React.Component {
+  handleChange() {
+    this.props.onUserInput(this.refs.filterTextInput.value);
+  }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          ref="filterTextInput"
+          onChange={this.handleChange.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    var onProductTableUpdate = this.props.onProductTableUpdate;
+    var rowDel = this.props.onRowDel;
+    var filterText = this.props.filterText;
+    var product = this.props.products.map(function (product) {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      return (
+        <ProductRow
+          onProductTableUpdate={onProductTableUpdate}
+          product={product}
+          onDelEvent={rowDel.bind(this)}
+          key={product.id}
+        />
+      );
+    });
+    return (
+      <div>
+        <button
+          type="button"
+          onClick={this.props.onRowAdd}
+          className="btn btn-success pull-right"
+        >
+          Add
+        </button>
+        <table className="table table-bordered fav">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>price</th>
+              <th>quantity</th>
+              <th>category</th>
+            </tr>
+          </thead>
+
+          <tbody>{product}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  onDelEvent() {
+    this.props.onDelEvent(this.props.product);
+  }
+  render() {
+    return (
+      <tr className="eachRow fav">
+        <EditableCell
+          onProductTableUpdate={this.props.onProductTableUpdate}
+          cellData={{
+            type: "name",
+            value: this.props.product.name,
+            id: this.props.product.id,
+          }}
+        />
+        <EditableCell
+          onProductTableUpdate={this.props.onProductTableUpdate}
+          cellData={{
+            type: "price",
+            value: this.props.product.price,
+            id: this.props.product.id,
+          }}
+        />
+        <EditableCell
+          onProductTableUpdate={this.props.onProductTableUpdate}
+          cellData={{
+            type: "qty",
+            value: this.props.product.qty,
+            id: this.props.product.id,
+          }}
+        />
+        <EditableCell
+          onProductTableUpdate={this.props.onProductTableUpdate}
+          cellData={{
+            type: "category",
+            value: this.props.product.category,
+            id: this.props.product.id,
+          }}
+        />
+        <td className="del-cell">
+          <input
+            type="button"
+            onClick={this.onDelEvent.bind(this)}
+            value="X"
+            className="del-btn"
+          />
+        </td>
+      </tr>
+    );
+  }
+}
+class EditableCell extends React.Component {
+  render() {
+    return (
+      <td>
+        <input
+          type="text"
+          name={this.props.cellData.type}
+          id={this.props.cellData.id}
+          value={this.props.cellData.value}
+          onChange={this.props.onProductTableUpdate}
+        />
+      </td>
+    );
+  }
+}
+ReactDOM.render(<Products />, document.getElementById("container"));
